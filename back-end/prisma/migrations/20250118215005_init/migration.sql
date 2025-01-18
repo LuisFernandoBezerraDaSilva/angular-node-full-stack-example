316228a2f5bdae544b1793b0a911872048a22f78
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-01-18 18:50:18 -0300

    seed creation

--- back-end/prisma/migrations/20250118215005_init/migration.sql
+++ back-end/prisma/migrations/20250118215005_init/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "tipo" INTEGER NOT NULL,
+    "role" INTEGER NOT NULL,
     "username" TEXT NOT NULL,
     "password" TEXT NOT NULL,
 
