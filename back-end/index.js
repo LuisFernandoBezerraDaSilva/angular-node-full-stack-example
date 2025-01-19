a8f1263c57069b5272e6192b2ffea873febae50e
Luis Fernando <luisfernandobezerradasilva@hotmail.com>
2025-01-19 03:37:02 -0300

    publishing adjustments

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,15 @@
+// filepath: /e:/projects/angular-node-full-stack-example/back-end/index.js
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
-const cors = require('cors'); // Importar o pacote cors
+const cors = require('cors');
 const routes = require('./routes/route');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const prisma = new PrismaClient();
-const PORT = 3000;
+const PORT = process.env.PORT
 
 app.use(cors());
 app.use(express.json());
